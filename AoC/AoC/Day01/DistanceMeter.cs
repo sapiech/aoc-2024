a1ef034385c3a5c0982e0964c028c8787f1f2a67
Przemyslaw Sapiecha <sapiech@live.com>
2024-12-01 23:36:50 +0100

    Day1

---
+++ AoC/AoC/Day01/DistanceMeter.cs
@@ -0,0 +1,62 @@
+﻿namespace AoC.Day01
+{
+    internal static class DistanceMeter
+    {
+        public async static Task<int> Meter()
+        {
+            var lists = await ReadLists();
+
+            lists.Left.Sort();
+            lists.Right.Sort();
+
+            var result = 0;
+
+            for (var i = 0; i < lists.Left.Count; i++)
+            {
+                result += Math.Abs(lists.Left[i] - lists.Right[i]);
+            }
+
+            return result;
+        }
+
+        public async static Task<int> MeterSimilarityScore()
+        {
+            var lists = await ReadLists();
+
+            var frequency = lists.Right.GroupBy(x => x)
+                .ToDictionary(x => x.Key, x => x.Count());
+
+            var result = 0;
+
+            foreach (var item in lists.Left)
+            {
+                if (frequency.TryGetValue(item, out var count))
+                {
+                    result += item * count;
+                }
+            }
+
+            return result;
+        }
+
+        private static async Task<Lists> ReadLists()
+        {
+            var lists = new Lists();
+
+            foreach (var line in await File.ReadAllLinesAsync("Day01/input.txt"))
+            {
+                var splitted = line.Split("   ");
+                lists.Left.Add(int.Parse(splitted[0]));
+                lists.Right.Add(int.Parse(splitted[1]));
+            }
+
+            return lists;
+        }
+    }
+
+    record Lists
+    {
+        public List<int> Left { get; } = [];
+        public List<int> Right { get; } = [];
+    }
+}
---
+++ AoC/AoC/Program.cs
@@ -0,0 +1,7 @@
+﻿using AoC.Day01;
+
+var day1part1 = await DistanceMeter.Meter();
+Console.WriteLine($"Total distance between lists is: {day1part1}.");
+
+var day1part2 = await DistanceMeter.MeterSimilarityScore();
+Console.WriteLine($"Sum of all similarity score is: {day1part2}.");
