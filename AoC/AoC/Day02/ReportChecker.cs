edb30b9e0f7b2ca02549febe08b11f7b1636da42
Przemyslaw Sapiecha <sapiech@live.com>
2024-12-02 18:37:59 +0100

    Day2

---
+++ AoC/AoC/Day02/ReportChecker.cs
@@ -0,0 +1,80 @@
+﻿namespace AoC.Day02
+{
+    internal static class ReportChecker
+    {
+        public static async Task<int> CheckReports(bool problemDampener)
+        {
+            int safeRecordsCount = 0;
+
+            await foreach (var line in File.ReadLinesAsync(@"Day02/input.txt"))
+            {
+                var level = line.Split(' ').Select(x => int.Parse(x)).ToList();
+                if (problemDampener)
+                {
+                    if (IsReportSafeWithProblemDampener(level))
+                    {
+                        safeRecordsCount++;
+                    }
+                }
+                else 
+                {
+                    if (IsReportSafe(level))
+                    {
+                        safeRecordsCount++;
+                    }
+                }
+            }
+
+            return safeRecordsCount;
+        }
+
+        private static bool IsReportSafeWithProblemDampener(List<int> level)
+        {
+            if (!IsReportSafe(level))
+            {
+                for (int i = 0; i < level.Count; i++)
+                {
+                    var workingCopy = new List<int>(level);
+                    workingCopy.RemoveAt(i);
+
+                    if (IsReportSafe(workingCopy))
+                    {
+                        return true;
+                    }
+                }
+            }
+            else 
+            { 
+                return true; 
+            }
+
+            return false;
+        }
+
+        private static bool IsReportSafe(List<int> levels)
+        {
+            bool? isIncreasing = null;
+
+            for (int i = 0; i < levels.Count - 1; i++)
+            {
+                int diff = levels[i + 1] - levels[i];
+
+                if (Math.Abs(diff) < 1 || Math.Abs(diff) > 3)
+                {
+                    return false;
+                }
+
+                if (isIncreasing is null)
+                {
+                    isIncreasing = diff > 0;
+                }
+                else if ((isIncreasing.Value && diff < 0) || (!isIncreasing.Value && diff > 0))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
--- AoC/AoC/Program.cs
+++ AoC/AoC/Program.cs
@@ -1,7 +1,14 @@
 ﻿using AoC.Day01;
+using AoC.Day02;
 
-var day1part1 = await DistanceMeter.Meter();
-Console.WriteLine($"Total distance between lists is: {day1part1}.");
+//var day1part1 = await DistanceMeter.Meter();
+//Console.WriteLine($"Total distance between lists is: {day1part1}.");
 
-var day1part2 = await DistanceMeter.MeterSimilarityScore();
-Console.WriteLine($"Sum of all similarity score is: {day1part2}.");
+//var day1part2 = await DistanceMeter.MeterSimilarityScore();
+//Console.WriteLine($"Sum of all similarity score is: {day1part2}.");
+
+var day2part1 = await ReportChecker.CheckReports(false);
+Console.WriteLine($"Total count of the safe reports is: {day2part1}.");
+
+var day2part2 = await ReportChecker.CheckReports(true);
+Console.WriteLine($"Total count of the safe reports handled by Problem Dampener is: {day2part2}.");
